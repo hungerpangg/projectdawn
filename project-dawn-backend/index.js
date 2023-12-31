5b4e4438bc1c1ebba216a2ac5f5a19cadb4a3a59
Hungerpangg <pangkawai22@gmail.com>
2023-12-31 17:22:21 +0800

    CORS issue

--- project-dawn-backend/index.js
+++ project-dawn-backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: ["http://localhost:3000"],
+		origin: ["https://projectdawn.onrender.com"],
 		methods: "GET,POST,PUT,DELETE", // The allowed HTTP methods
 		allowedHeaders: "Content-Type,Authorization", // The allowed request headers
 		credentials: true,
