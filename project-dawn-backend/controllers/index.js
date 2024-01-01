dbf9d4740e60728aefb573c8de8a88de51065b3e
Hungerpangg <pangkawai22@gmail.com>
2024-01-01 19:17:38 +0800

    Cookie issue

--- project-dawn-backend/controllers/index.js
+++ project-dawn-backend/controllers/index.js
@@ -116,7 +116,7 @@ module.exports.signup_post = async (req, res) => {
 			maxAge: maxAge * 1000,
 			secure: true,
 			sameSite: "None",
-			domain: "projectdawn.onrender.com",
+			domain: ".onrender.com",
 			path: "/",
 		});
 		const userId = user._id.toString();
@@ -223,7 +223,7 @@ module.exports.login = async (req, res) => {
 			maxAge: maxAge * 1000,
 			secure: true,
 			sameSite: "None",
-			domain: "projectdawn.onrender.com",
+			domain: ".onrender.com",
 			path: "/",
 		});
 		// var { email, _id, name } = user;
@@ -303,7 +303,7 @@ module.exports.logout = (req, res) => {
 		maxAge: 1,
 		secure: true,
 		sameSite: "None",
-		domain: "projectdawn.onrender.com",
+		domain: ".onrender.com",
 		path: "/",
 	});
 	res.status(200).json({ success: true });
